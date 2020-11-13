03da60d8a1814eb35eeff3fb424978bb0f9fa7cd
student <student@192.168.1.77>
2020-11-13 21:52:38 +0800

    SSH

---
+++ src/main/java/com/mycompany/springbasic1102/aop/Calc.java
@@ -0,0 +1,9 @@
+package com.mycompany.springbasic1102.aop;
+
+public interface Calc {
+    int add(int x, int y);
+    int sub(int x, int y);
+    int mul(int x, int y);
+    int div(int x, int y);
+            
+}
